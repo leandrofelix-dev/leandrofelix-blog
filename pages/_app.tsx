d4d41409ebd9ae680635de7d3158088ac67ecb29
leandrofelix-dev <lehinfo.felix@gmail.com>
2024-01-09 14:45:05 -0300

    feat: adicionando lib de SpeedInsight

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { Toaster } from 'react-hot-toast'
 import { Fira_Code, Inter } from 'next/font/google'
 import type { AppProps } from 'next/app'
 import 'src/styles/globals.css'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export const firaCode = Fira_Code({ subsets: ['latin'] })
 export const inter = Inter({ subsets: ['latin'] })
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Toaster />
+      <SpeedInsights />
       <Component {...pageProps} />
     </>
   )
