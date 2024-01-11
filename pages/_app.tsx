9fb0c9e6df1bb5220732a705340a7a7f6c571156
leandrofelix-dev <lehinfo.felix@gmail.com>
2024-01-10 22:44:45 -0300

    feat: adicionando Analytics ao projeto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { Fira_Code, Inter } from 'next/font/google'
 import type { AppProps } from 'next/app'
 import 'src/styles/globals.css'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/react'
 
 export const firaCode = Fira_Code({ subsets: ['latin'] })
 export const inter = Inter({ subsets: ['latin'] })
@@ -10,8 +11,9 @@ export const inter = Inter({ subsets: ['latin'] })
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Toaster />
       <SpeedInsights />
+      <Analytics />
+      <Toaster />
       <Component {...pageProps} />
     </>
   )
