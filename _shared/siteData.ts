8f147b70e9fc3084837fad66f56f4b6789129f63
leandrofelix-dev <lehinfo.felix@gmail.com>
2024-01-17 14:27:53 -0300

    feat: alterando dados no siteData

--- _shared/siteData.ts
+++ _shared/siteData.ts
@@ -6,11 +6,11 @@ const siteData = {
   footerText: '@ 2024 Leandro Felix Dev. Todos os direitos reservados. Feito com 💙.',
   language: 'pt-br',
   locale: 'pt-BR',
-  socialBanner: 'https://nextjs-notion-blog-starter.vercel.app/preview/1.jpg',
-  github: 'https://github.com/tuanphungcz/nextjs-notion-blog-starter',
-  twitter: 'https://github.com/tuanphungcz/nextjs-notion-blog-starter',
-  websiteUrl: 'https://nextjs-notion-blog-starter.vercel.app',
-  linkedin: 'https://github.com/tuanphungcz/nextjs-notion-blog-starter',
+  socialBanner: 'https://camo.githubusercontent.com/497eb6e11c3b1e84237930138dda8092b5b03c5af2c68652c3942d968102d57a/68747470733a2f2f692e6962622e636f2f635454307856462f636170612d6769746875622e706e67',
+  github: 'https://github.com/leandrofelix-dev/leandrofelix-blog',
+  twitter: 'https://twitter.com/leandrofelix_dev',
+  websiteUrl: 'https://blog.leandrofelix.dev.br',
+  linkedin: 'https://www.linkedin.com/in/leandrofelix-dev/',
 }
 
 export default siteData
